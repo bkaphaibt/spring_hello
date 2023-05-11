5ac1f12c6f908e4df53662dcea02cc6f71df689f
Bui Thanh Hai <bthai93@gmail.com>
2023-05-11 17:04:22 +0700

    sfsdfdf

--- JSPServlet_Form/src/main/java/controller/TestGitServlet.java
+++ JSPServlet_Form/src/main/java/controller/TestGitServlet.java
@@ -27,6 +27,7 @@ public class TestGitServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		System.out.println("Them du lieu de test conflict");
 		response.getWriter().append("Served at: ").append(request.getContextPath()+" , by Bui Thanh Hai test git");
 	}
 
